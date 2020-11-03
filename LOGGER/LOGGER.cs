bec6295b81979283bbc2c93cd7985108f8a91578
Weston Forbes <WestonForbes@automaticspring.com>
2020-11-03 11:25:16 -0500

    2020-11-03-01

---
+++ LOGGER/LOGGER.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+
+namespace FORBES
+{
+    public class LOGGER
+    {
+        //Objects
+        public int CONSOLE_VERBOSE = 9999;
+        public readonly DataTable LOG = new DataTable(); //The public event log for this class instance.
+        public static DataTable UNIFIED_LOG = new DataTable(); //The public event log for all instances in the current program.
+        public string ENTER_MESSAGE = ">>> Thread entering function.";
+        public string EXCEPTION_MESSAGE = "XXX Exception: {0}";
+        public string EXIT_FAIL_MESSAGE = "<<< Thread exiting function (fail).";
+        public string EXIT_SUCCESS_MESSAGE = "<<< Thread exiting function (success).";
+        public string INITIALIZE_MESSAGE = "^^^ Class initialized.";
+        private int ENTRY_ID = 0; //Holds how many entries are in the log. Used for entry ID purposes and overflow protection.
+        static int UNIFIED_ENTRY_ID = 0;
+
+        //Events
+        public event EventHandler LOG_RESET;
+        static event EventHandler ALL_LOGS_RESET;
+
+        //Constructor
+        public LOGGER(string LOG_NAME)
+        {
+            ADD_LOG_COLUMNS(); //Add all the columns to the DataTable. 
+            LOG.TableName = LOG_NAME; //Set the name of this instances log.
+            ALL_LOGS_RESET += new EventHandler(ALL_LOGS_RESET_EVENT);
+        }
+
+        //Methods
+
+        private void ALL_LOGS_RESET_EVENT(object sender, EventArgs E)
+        {
+            RESET_LOG();
+        } //When a static event to reset all logs triggers, each instance will clear their logs thru this delegate.
+        public void LOG_MESSAGE(int GROUP_ID, string MESSAGE, string EX_MESSAGE = null, [System.Runtime.CompilerServices.CallerMemberName] string FUNCTION = "")
+        {
+            OVERFLOW_PROTECT(); //Check to make sure the log isn't going to overflow.
+            int RES_MSG_FLAG = CHECK_IF_RESERVED_MESSAGE(ref MESSAGE, EX_MESSAGE); //Check if the message is one of the reserved messages.
+            DateTime TIME_STAMP = DateTime.Now; //Get the time.
+            string CONSOLE_MESSAGE = null; //Construct the console message.
+            CONSOLE_MESSAGE = LOG.TableName + ": "; //Construct the console message.
+            CONSOLE_MESSAGE += ENTRY_ID.ToString("00000000") + ": "; //Construct the console message.
+            CONSOLE_MESSAGE += TIME_STAMP.ToString("s") + ": "; //Construct the console message.
+            CONSOLE_MESSAGE += FUNCTION + ": " + MESSAGE; //Construct the console message.
+            if (MESSAGE == "") //Special case to output a blank console row, for readability reasons.
+                Console.WriteLine(""); //Write a blank line to the console.
+            else //If its not a special case...
+            {
+                UNIFIED_LOG.Rows.Add(LOG.TableName, UNIFIED_ENTRY_ID, ENTRY_ID, TIME_STAMP.ToString("s"), FUNCTION, GROUP_ID, MESSAGE, RES_MSG_FLAG); //Add the message to the log.
+                LOG.Rows.Add(LOG.TableName, UNIFIED_ENTRY_ID, ENTRY_ID, TIME_STAMP.ToString("s"), FUNCTION, GROUP_ID, MESSAGE, RES_MSG_FLAG); //Add the message to the log.
+                ENTRY_ID++; //Index up the ENTRY_ID. Don't omit/remove this as its used for overflow protection.
+                UNIFIED_ENTRY_ID++;
+                if (GROUP_ID <= CONSOLE_VERBOSE) Console.WriteLine(CONSOLE_MESSAGE); //Write the console message.
+            }
+        } //Main logging function.
+        public void RESET_LOG()
+        {
+            LOG.Clear();
+            ENTRY_ID = 0; //Reset the entry ID.
+            LOG_RESET?.Invoke(null, null); //Raise a event so that other classes can be notified that a reset happened.
+        } //Reset the log to its original state.
+        public static void RESET_ALL_LOGS()
+        {
+            UNIFIED_LOG.Clear();
+            UNIFIED_ENTRY_ID = 0;
+            ALL_LOGS_RESET?.Invoke(null, null); //Raise a event so that other classes can be notified that a reset happened.
+        } //Reset the log to its original state.
+        private int CHECK_IF_RESERVED_MESSAGE(ref string MESSAGE, string EX_MESSAGE = null)
+        {
+
+            switch (MESSAGE)
+            {
+                case ("ENTER"):
+                    MESSAGE = ENTER_MESSAGE;
+                    return 1;
+                case ("EXCEPTION"):
+                    MESSAGE = String.Format(EXCEPTION_MESSAGE, EX_MESSAGE);
+                    return 2;
+                case ("EXIT_FAIL"):
+                    MESSAGE = EXIT_FAIL_MESSAGE;
+                    return 3;
+                case ("EXIT_SUCCESS"):
+                    MESSAGE = EXIT_SUCCESS_MESSAGE;
+                    return 4;
+                case ("INITIALIZE"):
+                    MESSAGE = INITIALIZE_MESSAGE;
+                    return 5;
+                default:
+                    return 0;
+            }
+        } //Check if the message is reserved and needs to be substituted.
+        private void ADD_LOG_COLUMNS() //Adds the appropriate columns to the log.
+        {
+            List<string> COLUMN_NAMES = new List<string> 
+            {"Log Name", "Shared Entry ID", "Entry ID", "ISO 8601 TimeStamp", "Function", "Verbose Level", "Message", "Reserved Message Flag"};
+            DataColumnCollection COLLECTION = LOG.Columns;
+            DataColumnCollection UNIFIED_COLLECTION = UNIFIED_LOG.Columns;
+            foreach(string NAME in COLUMN_NAMES)
+            {
+                if (!COLLECTION.Contains(NAME))
+                    LOG.Columns.Add(NAME);
+                if (!UNIFIED_COLLECTION.Contains(NAME))
+                    UNIFIED_LOG.Columns.Add(NAME);
+            }
+
+        }
+        private void OVERFLOW_PROTECT() //Protects the log from overflowing.
+        {
+            //This function relies on ENTRY_ID being updated properly as doing a DataTable row count repeatedly would be a significant performance hit.
+            if (ENTRY_ID > 16000000) //16,777,216 is the maximum row count of a DataTable.
+            {
+                RESET_LOG();
+            }
+            if (UNIFIED_ENTRY_ID > 16000000) //16,777,216 is the maximum row count of a DataTable.
+            {
+                RESET_ALL_LOGS();
+            }
+        }
+    }
+}
+
---
+++ LOGGER/LOGGER_TEST_APPLICATION/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace LOGGER_TEST_APPLICATION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GRP_BOX_LOG = new System.Windows.Forms.GroupBox();
+            this.BTN_CLEAR_LOG = new System.Windows.Forms.Button();
+            this.LBL_VERBOSE = new System.Windows.Forms.Label();
+            this.DGV_LOG = new System.Windows.Forms.DataGridView();
+            this.TRCK_VERBOSE = new System.Windows.Forms.TrackBar();
+            this.BTN_TEST = new System.Windows.Forms.Button();
+            this.BTN_EXCEPTION = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.GRP_BOX_LOG.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_LOG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TRCK_VERBOSE)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GRP_BOX_LOG
+            // 
+            this.GRP_BOX_LOG.Controls.Add(this.BTN_CLEAR_LOG);
+            this.GRP_BOX_LOG.Controls.Add(this.LBL_VERBOSE);
+            this.GRP_BOX_LOG.Controls.Add(this.DGV_LOG);
+            this.GRP_BOX_LOG.Controls.Add(this.TRCK_VERBOSE);
+            this.GRP_BOX_LOG.Location = new System.Drawing.Point(20, 20);
+            this.GRP_BOX_LOG.Name = "GRP_BOX_LOG";
+            this.GRP_BOX_LOG.Size = new System.Drawing.Size(850, 330);
+            this.GRP_BOX_LOG.TabIndex = 113;
+            this.GRP_BOX_LOG.TabStop = false;
+            this.GRP_BOX_LOG.Text = "Log";
+            // 
+            // BTN_CLEAR_LOG
+            // 
+            this.BTN_CLEAR_LOG.Location = new System.Drawing.Point(665, 18);
+            this.BTN_CLEAR_LOG.Name = "BTN_CLEAR_LOG";
+            this.BTN_CLEAR_LOG.Size = new System.Drawing.Size(175, 25);
+            this.BTN_CLEAR_LOG.TabIndex = 7;
+            this.BTN_CLEAR_LOG.TabStop = false;
+            this.BTN_CLEAR_LOG.Text = "Clear Data Log";
+            this.BTN_CLEAR_LOG.UseVisualStyleBackColor = true;
+            // 
+            // LBL_VERBOSE
+            // 
+            this.LBL_VERBOSE.AutoSize = true;
+            this.LBL_VERBOSE.Location = new System.Drawing.Point(76, 24);
+            this.LBL_VERBOSE.Name = "LBL_VERBOSE";
+            this.LBL_VERBOSE.Size = new System.Drawing.Size(117, 13);
+            this.LBL_VERBOSE.TabIndex = 110;
+            this.LBL_VERBOSE.Text = "Verbose Detail Level: 3";
+            // 
+            // DGV_LOG
+            // 
+            this.DGV_LOG.AllowUserToAddRows = false;
+            this.DGV_LOG.AllowUserToDeleteRows = false;
+            this.DGV_LOG.AllowUserToResizeRows = false;
+            this.DGV_LOG.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV_LOG.Location = new System.Drawing.Point(10, 50);
+            this.DGV_LOG.Name = "DGV_LOG";
+            this.DGV_LOG.ReadOnly = true;
+            this.DGV_LOG.RowHeadersVisible = false;
+            this.DGV_LOG.Size = new System.Drawing.Size(830, 265);
+            this.DGV_LOG.TabIndex = 99;
+            // 
+            // TRCK_VERBOSE
+            // 
+            this.TRCK_VERBOSE.LargeChange = 1;
+            this.TRCK_VERBOSE.Location = new System.Drawing.Point(10, 20);
+            this.TRCK_VERBOSE.Maximum = 3;
+            this.TRCK_VERBOSE.Minimum = 1;
+            this.TRCK_VERBOSE.Name = "TRCK_VERBOSE";
+            this.TRCK_VERBOSE.Size = new System.Drawing.Size(60, 45);
+            this.TRCK_VERBOSE.TabIndex = 109;
+            this.TRCK_VERBOSE.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.TRCK_VERBOSE.Value = 3;
+            // 
+            // BTN_TEST
+            // 
+            this.BTN_TEST.Location = new System.Drawing.Point(20, 356);
+            this.BTN_TEST.Name = "BTN_TEST";
+            this.BTN_TEST.Size = new System.Drawing.Size(175, 25);
+            this.BTN_TEST.TabIndex = 111;
+            this.BTN_TEST.TabStop = false;
+            this.BTN_TEST.Text = "Test Main Log";
+            this.BTN_TEST.UseVisualStyleBackColor = true;
+            this.BTN_TEST.Click += new System.EventHandler(this.BTN_TEST_CLICK);
+            // 
+            // BTN_EXCEPTION
+            // 
+            this.BTN_EXCEPTION.Location = new System.Drawing.Point(201, 356);
+            this.BTN_EXCEPTION.Name = "BTN_EXCEPTION";
+            this.BTN_EXCEPTION.Size = new System.Drawing.Size(175, 25);
+            this.BTN_EXCEPTION.TabIndex = 114;
+            this.BTN_EXCEPTION.TabStop = false;
+            this.BTN_EXCEPTION.Text = "Generate Exception";
+            this.BTN_EXCEPTION.UseVisualStyleBackColor = true;
+            this.BTN_EXCEPTION.Click += new System.EventHandler(this.BTN_EXCEPTION_CLICK);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 356);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 25);
+            this.button1.TabIndex = 115;
+            this.button1.TabStop = false;
+            this.button1.Text = "Test Alternate Log";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.BTN_TEST_ALT_CLICK);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 411);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BTN_EXCEPTION);
+            this.Controls.Add(this.BTN_TEST);
+            this.Controls.Add(this.GRP_BOX_LOG);
+            this.Name = "Form1";
+            this.Text = "Logger Library Test Application";
+            this.GRP_BOX_LOG.ResumeLayout(false);
+            this.GRP_BOX_LOG.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV_LOG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TRCK_VERBOSE)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GRP_BOX_LOG;
+        private System.Windows.Forms.Button BTN_CLEAR_LOG;
+        private System.Windows.Forms.Label LBL_VERBOSE;
+        private System.Windows.Forms.DataGridView DGV_LOG;
+        private System.Windows.Forms.TrackBar TRCK_VERBOSE;
+        private System.Windows.Forms.Button BTN_TEST;
+        private System.Windows.Forms.Button BTN_EXCEPTION;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
